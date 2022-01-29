6e3e01815b45fcc04d61107363781a24f3822cde
Falkrik <tobaksduo@gmail.com>
2022-01-29 21:09:37 +0100

    Animations for vicotory and pause

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -8,13 +9,50 @@ public class UIManager : MonoBehaviour
 
     [Header("UI References")]
     [SerializeField] private Text timerText;
+    // Stock
+    // Duality count
+    [Space]
     [SerializeField] private Animator suddenDeathAnim;
+    [Space]
+    [SerializeField] private Text winnerText;
+    [SerializeField] private Animator winAnim;
+    [SerializeField] private GameObject replayPopup;
+    [SerializeField] private Image replayButtonHighlight;
+    [Space]
+    [SerializeField] private GameObject pauseParent;
+    [SerializeField] private GameObject pausePopup;
+    [SerializeField] private Image pauseButtonHighlight;
 
 
     [Header("Tools")]
     private int prevTime;
     private bool finalCountdownStarted = false;
+    private int pauseSelectedIndex = 0;
+    private int replaySelectedIndex = 0;
+    private bool gamePaused = false;
+    private bool gameOver = false;
 
+
+    private void Start()
+    {
+        //WinPopup(1);
+        //PauseScreen();
+    }
+
+    private void Update()
+    {
+        if (!gamePaused && !gameOver)
+            return;
+
+
+        if (Input.GetKeyUp(KeyCode.A))
+            SelectButton(-1);
+        if (Input.GetKeyUp(KeyCode.D))
+            SelectButton(1);
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return))
+            TriggerButton();
+    }
 
     public void UpdateStockCount(int playerNumber, int stockCount)
     {
@@ -71,12 +109,96 @@ public class UIManager : MonoBehaviour
 
     public void WinPopup(int playerNumber)
     {
+        gameOver = true;
+        winnerText.text = "Player " + playerNumber + " won!";
+        winAnim.Play("SuddenDeathPopup");
 
+        replaySelectedIndex = 0;
+        replayButtonHighlight.rectTransform.anchoredPosition = new Vector3(-140, -125, 0);
+        StartCoroutine(WinPopupCooldown());
     }
 
-    public void TransitionScene()
+    public void PauseScreen()
     {
+        if (gameOver)
+            return;
 
+        gamePaused = !gamePaused;
+
+        if(gamePaused)
+        {
+            pauseButtonHighlight.rectTransform.anchoredPosition = new Vector3(-276, -86, 0);
+            pauseSelectedIndex = 0;
+
+            pauseParent.SetActive(true);
+            pausePopup.GetComponent<Animator>().SetBool("Visible", true);
+        }
+        else
+        {
+            StartCoroutine(PausePopupCooldown());
+        }
     }
 
+    private void SelectButton(int dir)
+    {
+        if(gamePaused)
+        {
+            pauseSelectedIndex = (pauseSelectedIndex + dir < 0 ? 2 : pauseSelectedIndex + dir) % 3;
+            pauseButtonHighlight.rectTransform.anchoredPosition = new Vector3(-275 + 315 * pauseSelectedIndex, -86, 0);
+        }
+        else if(gameOver)
+        {
+            replaySelectedIndex = (replaySelectedIndex + dir < 0 ? 1 : replaySelectedIndex + dir) % 2;
+            replayButtonHighlight.rectTransform.anchoredPosition = new Vector3(-140 + 300 * replaySelectedIndex, -125, 0);
+        }
+    }
+
+    private void TriggerButton()
+    {
+        if(gamePaused)
+        {
+            switch(pauseSelectedIndex)
+            {
+                case 0:
+                    PauseScreen();
+                    break;
+
+                case 1:
+                    //
+                    break;
+
+                case 2:
+                    SceneManager.LoadScene(0);
+                    break;
+            }
+        }
+        else if(gameOver)
+        {
+            switch(replaySelectedIndex)
+            {
+                case 0:
+                    replayPopup.GetComponent<Animator>().SetBool("Visible", false);
+                    // GameManager.RefreshBattleManager();
+                    gameOver = false;
+                    break;
+
+                case 1:
+                    SceneManager.LoadScene(0);
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator WinPopupCooldown()
+    {
+        yield return new WaitForSeconds(3.5f);
+        replayPopup.GetComponent<Animator>().SetBool("Visible", true);
+    }
+
+    private IEnumerator PausePopupCooldown()
+    {
+        pausePopup.GetComponent<Animator>().SetBool("Visible", false);
+        yield return new WaitForSeconds(0.8f);
+        pauseParent.SetActive(false);
+    }
 }
