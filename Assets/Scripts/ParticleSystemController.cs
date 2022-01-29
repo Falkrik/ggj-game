788a16554c9349d3eda3ffbd594b84be52f0a65a
Falkrik <tobaksduo@gmail.com>
2022-01-29 15:40:00 +0100

    Particle System implemented, without particles

--- Assets/Scripts/ParticleSystemController.cs
+++ Assets/Scripts/ParticleSystemController.cs
@@ -6,38 +6,64 @@ public class ParticleSystemController : MonoBehaviour
 {
     [Header("Particle Prefabs")]
     public GameObject deathParticlePrefab;
+    [Space]
+    public GameObject dualityParticlePrefab;
+    [Space]
+    public GameObject jumpParticlePrefab;
+    [Space]
+    public GameObject doublejumpParticlePrefab;
+    [Space]
+    public GameObject hitgroundParticlePrefab;
+    [Space]
+    public GameObject phaseplatformParticlePrefab;
+
 
     public static ParticleSystemController particleContoller;
+
+    private void Awake() { particleContoller = this; }
 
 
     public void SpawnParticleSystem(ParticleType type, Vector3 position)
     {
+        GameObject particle = null;
+
         switch(type)
         {
             case ParticleType.DEATH:
-                //
+                particle = deathParticlePrefab;
                 break;
 
             case ParticleType.DUALITY:
-                //
+                particle = dualityParticlePrefab;
                 break;
 
             case ParticleType.JUMP:
-                //
+                particle = jumpParticlePrefab;
                 break;
 
             case ParticleType.DOUBLEJUMP:
-                //
+                particle = doublejumpParticlePrefab;
                 break;
 
             case ParticleType.HITGROUND:
-                //
+                particle = hitgroundParticlePrefab;
                 break;
 
             case ParticleType.PHASEPLATFORM:
-                //
+                particle = phaseplatformParticlePrefab;
                 break;
+
+            default:
+                return;
         }
+
+        if(particle == null)
+        {
+            Debug.LogError("ParticleSystemController: Particle Prefab was not added to ParticleSystemController");
+            return;
+        }
+
+        Instantiate(particle, position, Quaternion.identity);
     }
 
 }
