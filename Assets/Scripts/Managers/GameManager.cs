cd901c80cf4b2976ccf1773f73e6eeaae5908438
Ace Derrick Young <luadengames@gmail.com>
2022-01-29 11:32:03 +0100

    Player/Character Base Functionality, BattleManager/GameManager Update

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,13 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    protected BattleManager battleManager;
+    [SerializeField] private BattleManager battleManager;
+
+    [SerializeField] private int playerCount;
+    [SerializeField] private int playerStockCount;
+    [SerializeField] private float maxMatchTime;
+
+    public static GameManager gameManager;
     
+    public int PlayerCount { get => playerCount; }
+    public int PlayerStockCount { get => playerStockCount; }
+    public float MaxMatchTime { get => maxMatchTime; }
+
+    public void Start()
+    {
+        InitGameManager();
+    }
+
+    private void InitGameManager()
+    {
+        if (battleManager == null)
+            throw new NotImplementedException("BattleManager object was not added to the GameManager.");
+
+
+    }
+
     public void RefreshBattleManager()
     {
+        battleManager.InitBattleManager();
         return;
     }
 
---
+++ Assets/Scripts/Objects/Character.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Character : MonoBehaviour
+{
+    protected int playerNumber;
+    protected Player player;
+    protected Rigidbody2D rb;
+
+    public Player CharacterPlayer { get => player; }
+
+    public Character(Player characterPlayer)
+    {
+        player = characterPlayer;
+        playerNumber = player.PlayerNumber;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void MoveCharacter(Vector2 moveDir)
+    {
+        //Add animation controls here.
+        rb.AddForce(moveDir * moveSpeed, ForceMode2D.Force);
+    }
+
+    public void Jump(Vector2 jumpForce)
+    {
+        rb.AddForce(jumpForce, ForceMode2D.Impulse);
+        //Add animation and particle effects here.
+    }
+
+    public void ResetJumpCount() => currentJumpCount = 0;
+
+    public void UsePush()
+    {
+        //Complete after.
+    }
+
+    public void UseDuality()
+    {
+        //Complete after.
+    }
+
+    public void 
+}
